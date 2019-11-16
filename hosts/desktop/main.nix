ad1a05a72d291d5b5b2768d8ac81e2fb1417b414
syberant <sybrand@neuralcoding.com>
2019-11-16 09:29:31 +0100

    merge desktop-specific files

---
+++ hosts/desktop/main.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  # Use the GRUB 2 boot loader.
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.useOSProber = true;
+
+  # Mount additional filesystems
+  fileSystems."/home".device = "/dev/disk/by-label/home";
+
+  networking.hostName = "nixos-desktop"; # Define your hostname.
+
+  # Get Xserver working
+  services.xserver.videoDrivers = [ "intel" ];
+  boot.kernelModules = [ "i915" ];
+}
