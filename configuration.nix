988ae8f51e4897e6c650386608c06c3484fb459f
syberant <sybrand@neuralcoding.com>
2020-05-22 10:10:44 +0200

    Clean up configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -22,15 +22,11 @@ in {
       ./modules/default.nix
     ];
 
-
   # Configure overlays
   nixpkgs.overlays = [
     (import ./overlays/added_packages)
     (import ./overlays/explicit_configuration)
   ];
-
-  # My custom derivations live here
-  environment.extraInit = "export NIX_PATH=custompkgs=/etc/nixos/custompkgs/default.nix:$NIX_PATH";
 
   programs.rupa_z.enable = true;
 
@@ -47,22 +43,8 @@ in {
   networking.useDHCP = false;
   #networking.interfaces.wlp3s0.useDHCP = true;
 
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -105,8 +87,6 @@ in {
     extraOptions = [ "noevents" "idle 2" ];
   };
 
-  # Keyboard delay
-  services.xserver.autoRepeatDelay = 250;
 
   # GPG config
   programs.gnupg.agent.enable = true;
@@ -114,6 +94,9 @@ in {
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
+
+    # Keyboard delay
+    autoRepeatDelay = 250;
   };
 
   # hardware.opengl.driSupport32Bit = true;
