0cb73bf98ace8ca5cfc2e5c0eae3142f7387d872
syberant <sybrand@neuralcoding.com>
2020-05-25 10:23:28 +0200

    Move autostart of sxhkd to i3-config-file.nix

--- configuration/desktop-environment/i3-config-file.nix
+++ configuration/desktop-environment/i3-config-file.nix
@@ -1,4 +1,5 @@
 builtins.toFile "i3-config-file" (
 	builtins.readFile ../../config/i3/config +
-	"exec_always --no-startup-id polybar example &"
+	"exec_always --no-startup-id polybar example &" +
+	"exec sxhkd &"
 )
