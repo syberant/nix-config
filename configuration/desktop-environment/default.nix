2260da59f55fb9847bbfaff6bb9bab5b9d3a135b
syberant <sybrand@neuralcoding.com>
2020-07-11 14:47:03 +0200

    Refactor common DE functionality into common.nix Full path: configuration/desktop-environment/common.nix

--- configuration/desktop-environment/default.nix
+++ configuration/desktop-environment/default.nix
@@ -1,48 +1,22 @@
 { pkgs, config, ... }:
 
-let lib = pkgs.lib;
-in {
-  imports = [ ];
+{
+  imports = [ ./common.nix ];
 
-  # Hide mouse after a while
-  services.unclutter = {
-    enable = true;
-    extraOptions = [ "noevents" "idle 2" ];
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-
-    # Keyboard delay
-    autoRepeatDelay = 250;
-    xkbOptions = "compose:ralt";
-
-    # Enable i3 as desktop/window manager
-    windowManager.i3 = {
-	enable = true;
-	configFile = pkgs.writeText "i3-config-file" (
-		builtins.readFile ../dotfiles/i3/config +
-		"exec_always --no-startup-id ${pkgs.polybar}/bin/polybar -c ${import ../generators/polybar.nix { config = config.systemInfo; }} example &" +
-		"exec ${pkgs.sxhkd}/bin/sxhkd -c ${import ../generators/sxhkdrc.nix {inherit pkgs;}} &" +
-		"exec ${pkgs.dunst}/bin/dunst -config ${../dotfiles/dunst/config} &"
-	);
-	package = pkgs.i3-gaps;
-	extraPackages = with pkgs; [
-	  dmenu
-	  feh
-	  rxvt_unicode
-	];
-    };
-  };
-
-  # Enable compton
-  services.compton = {
-    enable = true;
-    opacityRules = [
-      "90:class_g = 'st-256color'"
-      # "90:class_g = 'st-256color' && enabled"
-      # "70:class_g = 'st-256color' && !enabled"
-    ];
+  # Enable i3 as desktop/window manager
+  services.xserver.windowManager.i3 = {
+      enable = true;
+      configFile = pkgs.writeText "i3-config-file" (
+              builtins.readFile ../dotfiles/i3/config +
+              "exec_always --no-startup-id ${pkgs.polybar}/bin/polybar -c ${import ../generators/polybar.nix { config = config.systemInfo; }} example &" +
+              "exec ${pkgs.sxhkd}/bin/sxhkd -c ${import ../generators/sxhkdrc.nix {inherit pkgs;}} &" +
+              "exec ${pkgs.dunst}/bin/dunst -config ${../dotfiles/dunst/config} &"
+      );
+      package = pkgs.i3-gaps;
+      extraPackages = with pkgs; [
+        dmenu
+        feh
+        rxvt_unicode
+      ];
   };
 }
