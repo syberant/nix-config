42d1d49b485a9ba157b428d99769dd4aaaf69d6d
syberant <sybrand@neuralcoding.com>
2020-07-13 00:09:52 +0200

    Configure dwm to be more like i3 config - Keybindings - Applied own viewcumulative patch

--- configuration/desktop-environment/dwm.nix
+++ configuration/desktop-environment/dwm.nix
@@ -1,11 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 let dwm = pkgs.dwm.overrideAttrs (old: {
+  #src = /home/sybrand/Documents/Programmeren/C/dwm;
+
   postPatch = ''
     cp ${../dotfiles/dwm/config.h} ./config.h
   '';
 
   patches = [
+    ../dotfiles/dwm/dwm-viewcumulative-6.2.diff
+
+    # Considered patches:
+    # namedscratchpads
+    # swallow
+    # keymodes
   ];
 });
 in {
@@ -15,6 +23,11 @@ in {
     start = ''
       ${dwm}/bin/dwm &
       waitPID=$!
+
+      #${pkgs.polybar}/bin/polybar -c ${import ../generators/polybar.nix { config = config.systemInfo; }} example &
+      ${pkgs.sxhkd}/bin/sxhkd -c ${import ../generators/sxhkdrc.nix { inherit pkgs; }} &
+      ${pkgs.dunst}/bin/dunst -config ${../dotfiles/dunst/config} &
+      ~/.fehbg
     '';
   }];
 }
--- configuration/dotfiles/dwm/config.h
+++ configuration/dotfiles/dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -44,7 +44,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask // Super key
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -61,8 +61,8 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -70,13 +70,13 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	//{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
@@ -94,6 +94,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	//custom
+	{ MODKEY|ControlMask,           XK_j,      viewcumulative, {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_k,      viewcumulative, {.i = +1 } },
+	{ ControlMask,                  XK_Left,   viewcumulative, {.i = -1 } },
+	{ ControlMask,                  XK_Right,  viewcumulative, {.i = +1 } },
 };
 
 /* button definitions */
