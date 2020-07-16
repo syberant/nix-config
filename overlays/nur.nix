f56341f15de6401117031ba1c093c4f165bed514
syberant <sybrand@neuralcoding.com>
2020-07-16 15:43:27 +0200

    Update NUR to include swallow patch

--- overlays/nur.nix
+++ overlays/nur.nix
@@ -1,9 +1,12 @@
 self: super: {
   nur = import (builtins.fetchTarball
-    (let rev = "d0468eaa0066a011f87122bbfb13d3ba619e742f";
+    (let rev = "4bd81468c6203e19c71576f89a9b356ac79646c4";
     in {
       url = "https://github.com/nix-community/NUR/archive/${rev}.tar.gz";
-      sha256 = "0zzjhff2nmykwc31ryaa3gcff6pp0cx4zf5am4569gh3a3mwplsr";
+      sha256 = "04nwssxiiagkzz2gqm75wlk8qm3y2ra4fl9lczgbzkz7k8ih6d8v";
     })
-  ) { pkgs = super; };
+    ) {
+      pkgs = super;
+      #repoOverrides = { syberant = import /home/sybrand/Documents/Programmeren/Nix/nur-packages { pkgs = super; }; };
+    };
 }
