0cbd8eb1279c37c4374e2095ddff8e9d6fda11bf
syberant <sybrand@neuralcoding.com>
2020-07-27 12:59:57 +0200

    Re-enable nixos-hardware

--- hosts/macbook/main.nix
+++ hosts/macbook/main.nix
@@ -4,7 +4,7 @@ let nixos-hardware = (import ../../nix/sources.nix).nixos-hardware;
 in {
   imports = [
     ./sync.nix
-    #(nixos-hardware + "/apple/macbook-air/6")
+    (nixos-hardware + "/apple/macbook-air/6")
     ./hardware-configuration.nix
     ../../configuration/common.nix
   ];
@@ -33,9 +33,15 @@ in {
   hardware.facetimehd.enable = true;
 
   # Battery life tweaks
+  # - view usage with powerstat
   services.tlp = {
     enable = true;
     extraConfig = ''
+      # force battery mode even on AC
+      TLP_DEFAULT_MODE=BAT
+      TLP_PERSISTANT_DEFAULT=1
+
+      # manually set performance policy
       ENERGY_PERF_POLICY_ON_BAT=power
       ENERGY_PERF_POLICY_ON_AC=balance-performance
     '';
