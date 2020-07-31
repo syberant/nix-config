de41a59cc015638ad6726722567537f09a230362
syberant <sybrand@neuralcoding.com>
2020-07-31 10:40:32 +0200

    Add fzf shell keybindings

--- configuration/packages/programs.nix
+++ configuration/packages/programs.nix
@@ -1,9 +1,15 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   programs = {
     bash.interactiveShellInit = builtins.readFile ../dotfiles/bash/bashrc +
-      lib.optionalString (builtins.pathExists ../dotfiles/bash/bash_aliases) ". ${../dotfiles/bash/bash_aliases}";
+    lib.optionalString (builtins.pathExists ../dotfiles/bash/bash_aliases) ''
+      . ${../dotfiles/bash/bash_aliases}
+    '' + ''
+      # Enable cool fzf functionality such as ctrl+r history searching
+      source ${pkgs.fzf}/share/fzf/completion.bash
+      source ${pkgs.fzf}/share/fzf/key-bindings.bash
+    '';
 
     rupa_z.enable = true;
 
