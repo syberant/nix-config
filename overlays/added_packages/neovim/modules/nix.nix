2fdc0d8fb820b99a51227c2111dfa130d5067ff8
syberant <sybrand@neuralcoding.com>
2020-08-04 00:06:25 +0200

    Make neovim autoformat nix files

--- overlays/added_packages/neovim/modules/nix.nix
+++ overlays/added_packages/neovim/modules/nix.nix
@@ -1,9 +1,13 @@
 { pkgs, ... }:
 
 {
-	plugins = with pkgs.vimPlugins; [
-		vim-nix
-	];
+  plugins = with pkgs.vimPlugins; [ vim-nix neoformat ];
 
-	code = "";
+  # TODO: integrate dependency on nixfmt into this module
+  code = ''
+    augroup nix
+      autocmd!
+      autocmd BufWritePre *.nix undojoin | Neoformat nixfmt
+    augroup END
+  '';
 }
