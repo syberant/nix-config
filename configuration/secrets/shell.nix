0a3b7cdbc31f1aff9e184244f63832b4395c76f8
syberant <sybrand@neuralcoding.com>
2020-08-04 16:33:21 +0200

    Ease importing ssh keys via nix-shell

--- configuration/secrets/shell.nix
+++ configuration/secrets/shell.nix
@@ -6,7 +6,13 @@ in pkgs.mkShell {
   # Directory containing public keys
   sopsPGPKeyDirs = [ ./keys ];
 
-  # Automatically set SOPS_PGP_FP with above public keys
-  # Allows editing the secrets.yaml file
-  nativeBuildInputs = with sops; [ sops-pgp-hook ];
+  nativeBuildInputs = with sops;
+    [
+      # Automatically set SOPS_PGP_FP with above public keys
+      # Allows editing the secrets.yaml file
+      sops-pgp-hook
+
+      # Import ssh keys to gpg for new hosts, takes some time to build
+      #ssh-to-pgp
+    ];
 }
