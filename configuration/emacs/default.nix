cab23e7ff491f7d89b9fa2168f6f27324851b689
syberant <sybrand@neuralcoding.com>
2020-08-05 14:07:54 +0200

    Use .config/emacs/init.el as .emacs symlink

--- configuration/emacs/default.nix
+++ configuration/emacs/default.nix
@@ -5,4 +5,8 @@
     enable = true;
     package = import ./package.nix { inherit pkgs; };
   };
+
+  home-manager.users.sybrand.home.file.".emacs" = {
+    source = ../linkFiles/.config/emacs/init.el;
+  };
 }
