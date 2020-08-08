9703418a772b33586a6c0b0a7dd4d9e03f527d4f
syberant <sybrand@neuralcoding.com>
2020-08-08 11:19:38 +0200

    nixfmt

--- configuration/packages/overview.nix
+++ configuration/packages/overview.nix
@@ -3,92 +3,91 @@
 {
   imports = [ ./programs.nix ];
 
-  environment.systemPackages = with pkgs;
-    [
-      # Some defaults
-      curl
-      wget
-      vim
-      git
-      tree
-      tmux
+  environment.systemPackages = with pkgs; [
+    # Some defaults
+    curl
+    wget
+    vim
+    git
+    tree
+    tmux
 
-      # Security
-      gnupg
-      pass
+    # Security
+    gnupg
+    pass
 
-      # Development prerequisites
-      gnumake
-      gcc
+    # Development prerequisites
+    gnumake
+    gcc
 
-      # Personal development
-      zeal
-      tokei
-      myNeovim
-      niv
+    # Personal development
+    zeal
+    tokei
+    myNeovim
+    niv
 
-      # Personal choices
-      nnn
-      lf
-      st
-      unison
-      yadm
+    # Personal choices
+    nnn
+    lf
+    st
+    unison
+    yadm
 
-      # Rust coreutils
-      lsd
-      bat
-      ripgrep
-      fd
-      du-dust
+    # Rust coreutils
+    lsd
+    bat
+    ripgrep
+    fd
+    du-dust
 
-      # Fluff
-      neofetch
-      cmatrix
-      xdotool
-      htop
-      ytop
+    # Fluff
+    neofetch
+    cmatrix
+    xdotool
+    htop
+    ytop
 
-      # Relaxing
-      newsboat
-      mpv
+    # Relaxing
+    newsboat
+    mpv
 
-      # Utilities for UX
-      xorg.xrandr
-      redshift
+    # Utilities for UX
+    xorg.xrandr
+    redshift
 
-      # Utilities for CLI UX
-      fzf
-      tig
-      entr
-      starship
+    # Utilities for CLI UX
+    fzf
+    tig
+    entr
+    starship
 
-      # Hardware debugging utilities
-      pciutils
-      usbutils
+    # Hardware debugging utilities
+    pciutils
+    usbutils
 
-      # Software debugging
-      xorg.xev
+    # Software debugging
+    xorg.xev
 
-      # Programming languages
-      python3
-      rustup
-      octave
-      #swift # Doesn't work on latest unstable
+    # Programming languages
+    python3
+    rustup
+    octave
+    #swift # Doesn't work on latest unstable
 
-      # Formatters
-      nixfmt
+    # Formatters
+    nixfmt
 
-      # Graphical defaults
-      zathura
-      signal-desktop
-      sxiv
-      chromium
-      thunderbird
+    # Graphical defaults
+    zathura
+    signal-desktop
+    sxiv
+    chromium
+    thunderbird
 
-      # Productivity
-      texlive.combined.scheme-full
-      anki
-      unzip
-      libreoffice
-    ];
+    # Productivity
+    texlive.combined.scheme-full
+    anki
+    unzip
+    libreoffice
+  ];
 }
