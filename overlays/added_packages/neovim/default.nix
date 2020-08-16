3e7f8e16c9a276b3f11b5fd1c9c2feb234b5bd28
syberant <sybrand@neuralcoding.com>
2020-08-16 12:55:21 +0200

    rustfmt

--- overlays/added_packages/neovim/default.nix
+++ overlays/added_packages/neovim/default.nix
@@ -4,18 +4,18 @@
 { pkgs, ... }:
 
 pkgs.neovim.override {
-    configure = let
-    	modules = [
-		(import ./modules/main.nix {inherit pkgs;})
-		(import ./modules/markdown.nix {inherit pkgs;})
-		(import ./modules/nix.nix {inherit pkgs;})
-		(import ./modules/rust.nix {inherit pkgs;})
-	];
-	plugins = builtins.foldl' (a: b: a ++ b.plugins) [] modules;
-	code = builtins.foldl' (a: b: a + "\n" + b.code) "" modules;
-    in {
-        packages.myVimPackage.start = plugins;
+  configure = let
+    modules = [
+      (import ./modules/main.nix { inherit pkgs; })
+      (import ./modules/markdown.nix { inherit pkgs; })
+      (import ./modules/nix.nix { inherit pkgs; })
+      (import ./modules/rust.nix { inherit pkgs; })
+    ];
+    plugins = builtins.foldl' (a: b: a ++ b.plugins) [ ] modules;
+    code = builtins.foldl' (a: b: a + "\n" + b.code) "" modules;
+  in {
+    packages.myVimPackage.start = plugins;
 
-        customRC = code;
-    };
+    customRC = code;
+  };
 }
