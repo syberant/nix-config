9eb5b13bc38ac77ace684ce8a1da841126abe961
syberant <sybrand@neuralcoding.com>
2020-10-26 16:20:33 +0100

    XMonad: put mouseFollowsFocus into .xmonad/lib

--- configuration/desktop-environment/xmonad/xmonad.hs
+++ configuration/desktop-environment/xmonad/xmonad.hs
@@ -10,33 +10,15 @@ import System.IO
 import qualified Data.Map as Map
 import XMonad.StackSet (focusDown, focusUp)
 import XMonad.Actions.CycleWS (nextWS, prevWS)
-import Foreign.C.Types (CInt (..))
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.NamedScratchpad (NamedScratchpad (NS), namedScratchpadAction, namedScratchpadManageHook)
 import qualified XMonad.Util.NamedScratchpad as NS
 import qualified XMonad.StackSet as W
 
+-- Own modules (well, for now just shamelessly ripped from splintah's config)
+import MouseFollowsFocus (mouseFollowsFocus)
+
 myTerminal = "st"
-
--- | Move the mouse pointer to the centre of the window.
-mouseToWindowCentre :: Display -> Window -> X ()
-mouseToWindowCentre display window = liftIO $ do
-  WindowAttributes {wa_width = width, wa_height = height} <-
-    getWindowAttributes display window
-  -- 'warpPointer' moves the mouse pointer relative to the origin of the
-  -- destination window (the third argument, here 'window'). The x and y
-  -- coordinates of the focused window are thus not needed to move the mouse
-  -- pointer to the centre of the window.
-  let CInt x = width  `div` 2
-  let CInt y = height `div` 2
-  warpPointer display 0 window 0 0 0 0 x y
-
--- | Move the mouse pointer to the centre of the focused window.
-mouseFollowsFocus :: X ()
-mouseFollowsFocus =
-  withFocused $ \window ->
-  withDisplay $ \display ->
-  mouseToWindowCentre display window
 
 myKeys conf@XConfig {XMonad.modMask = modm} = Map.fromList [
   ---- Applications
