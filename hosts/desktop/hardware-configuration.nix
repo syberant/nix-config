9c56dcded0aa355c3ac1031f548e23be8760aa80
syberant <sybrand@neuralcoding.com>
2021-04-19 11:00:37 +0200

    nixos-desktop: Add temperature sensor kernel module

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "nct6775" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
