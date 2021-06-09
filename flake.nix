3631adf41c5ea1da9adf064a76d6e62604861eae
syberant <sybrand@neuralcoding.com>
2021-06-09 12:23:08 +0200

    Fix `nixosConfigurations` in flake.nix

--- flake.nix
+++ flake.nix
@@ -37,84 +37,83 @@
   outputs = { self, flake-utils, nixpkgs, nixpkgs-git, nixos-hardware, NUR
     , home-manager, sops-nix, nix-neovim, secrets }:
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        specialArgs = {
-          inherit self nixos-hardware nix-neovim secrets;
+    let
+      # TODO: utilise flake-utils for this
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit self nixos-hardware nix-neovim secrets;
 
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-            overlays =
-              [ NUR.overlay (import ./overlays/explicit_configuration) ];
-          };
-
-          nixpkgs-git = import nixpkgs-git {
-            inherit system;
-            config.allowUnfree = true;
-          };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ NUR.overlay (import ./overlays/explicit_configuration) ];
         };
-        # https://github.com/cideM/dotfiles/blob/master/flake.nix
-        hm-nixos-as-super = { config, ... }: {
-          # Submodules have merge semantics, making it possible to amend
-          # the `home-manager.users` submodule for additional functionality.
-          options.home-manager.users = let lib = specialArgs.pkgs.lib;
-          in lib.mkOption {
-            type = lib.types.attrsOf (lib.types.submoduleWith {
-              modules = [ ];
-              # Makes specialArgs available to Home Manager modules as well.
-              specialArgs = specialArgs // {
-                # Allow accessing the parent NixOS configuration.
-                super = config;
-              };
-            });
-          };
-        };
-        sharedModule = {
-          imports = [
-            sops-nix.nixosModules.sops
-            home-manager.nixosModules.home-manager
-            hm-nixos-as-super
-          ];
 
-          # Pin nixpkgs in registry
-          nix.registry.nixpkgs.flake = nixpkgs;
-
-          # Pin NIX_PATH
-          nix.nixPath = [
-            "nixpkgs=${nixpkgs}"
-            "nixos-config=/etc/nixos/configuration.nix"
-          ];
+        nixpkgs-git = import nixpkgs-git {
+          inherit system;
+          config.allowUnfree = true;
         };
+      };
+      # https://github.com/cideM/dotfiles/blob/master/flake.nix
+      hm-nixos-as-super = { config, ... }: {
+        # Submodules have merge semantics, making it possible to amend
+        # the `home-manager.users` submodule for additional functionality.
+        options.home-manager.users = let lib = specialArgs.pkgs.lib;
+        in lib.mkOption {
+          type = lib.types.attrsOf (lib.types.submoduleWith {
+            modules = [ ];
+            # Makes specialArgs available to Home Manager modules as well.
+            specialArgs = specialArgs // {
+              # Allow accessing the parent NixOS configuration.
+              super = config;
+            };
+          });
+        };
+      };
+      sharedModule = {
+        imports = [
+          sops-nix.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          hm-nixos-as-super
+        ];
+
+        # Pin nixpkgs in registry
+        nix.registry.nixpkgs.flake = nixpkgs;
+
+        # Pin NIX_PATH
+        nix.nixPath =
+          [ "nixpkgs=${nixpkgs}" "nixos-config=/etc/nixos/configuration.nix" ];
+      };
+    in {
+      nixosConfigurations.nixos-macbook = nixpkgs.lib.nixosSystem {
+        inherit specialArgs system;
+
+        modules = [ ./hosts/macbook/main.nix sharedModule ];
+      };
+
+      nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
+        inherit specialArgs system;
+
+        modules = [ ./hosts/desktop/main.nix sharedModule ];
+      };
+    } // flake-utils.lib.eachDefaultSystem (system: {
+      apps = let
+        configuration = {
+          imports =
+            [ ./configuration/home-manager/modules/neovim/configuration.nix ];
+        };
+        bin = nix-neovim.fromConfig configuration;
+        mkApp = flake-utils.lib.mkApp;
+        pkgs = import nixpkgs { inherit system; };
       in {
-        nixosConfigurations.nixos-macbook = nixpkgs.lib.nixosSystem {
-          inherit specialArgs system;
+        neovim = mkApp { drv = bin; };
 
-          modules = [ ./hosts/macbook/main.nix sharedModule ];
+        neovim-debug = mkApp {
+          drv = pkgs.writeScriptBin "neovim-debug" ''
+            echo 'Opening ${bin.passthru.customRC}'
+            ${bin}/bin/nvim ${bin.passthru.customRC} $@
+          '';
         };
-
-        nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
-          inherit specialArgs system;
-
-          modules = [ ./hosts/desktop/main.nix sharedModule ];
-        };
-
-        apps = let
-          configuration = {
-            imports =
-              [ ./configuration/home-manager/modules/neovim/configuration.nix ];
-          };
-          bin = nix-neovim.fromConfig configuration;
-          mkApp = flake-utils.lib.mkApp;
-        in {
-          neovim = mkApp { drv = bin; };
-
-          neovim-debug = mkApp {
-            drv = specialArgs.pkgs.writeScriptBin "neovim-debug" ''
-              echo 'Opening ${bin.passthru.customRC}'
-              ${bin}/bin/nvim ${bin.passthru.customRC} $@
-            '';
-          };
-        };
-      });
+      };
+    });
 }
