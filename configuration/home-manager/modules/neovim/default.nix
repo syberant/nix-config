ac066ce766c00e1bab22456429750620161b9b04
syberant <sybrand@neuralcoding.com>
2021-06-30 18:04:31 +0200

    Install neovide

--- configuration/home-manager/modules/neovim/default.nix
+++ configuration/home-manager/modules/neovim/default.nix
@@ -1,4 +1,14 @@
-{ pkgs, nix-neovim, ... }:
+{ pkgs, nixpkgs-git, nix-neovim, ... }:
 
 let myNeovim = nix-neovim.fromConfig ./configuration.nix;
-in { home.packages = [ myNeovim ]; }
+in {
+  home.packages = [
+    myNeovim 
+
+    # Override necessary here
+    # Commented for now to try out neovide
+    # (pkgs.neovim-qt.override { neovim = myNeovim; })
+
+    nixpkgs-git.neovide
+  ];
+}
