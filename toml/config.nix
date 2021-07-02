a424ae0ccab1c99fe90e5e5862a06867cb2ae243
syberant <sybrand@neuralcoding.com>
2021-07-02 23:52:48 +0200

    Remove `toml/config.nix`
    Moved functionality into `toml/default.nix`

--- toml/config.nix
+++
@@ -1,8 +0,0 @@
-{ lib, pkgs, ... }:
-
-with pkgs.nur.repos.syberant.lib;
-
-{
-  # TODO: Output generated configuration somewhere for transparency
-  imports = map importToml (getTomlFiles ./config);
-}
--- toml/default.nix
+++ toml/default.nix
@@ -1,16 +1,22 @@
 { lib, pkgs, nixpkgs-git, ... }:
 
 with lib;
+with pkgs.nur.repos.syberant.lib;
 
-let
-  recurse = p: list: let h = p.${head list}; t = tail list; in if (t == [ ]) then h else recurse h t;
-  getPkg = packages: string: recurse packages (splitString "." string);
-  fromList = { file, packages }:
-    map (getPkg packages) (fromTOML (readFile file)).packages;
-in {
-  imports = [ ./config.nix ];
+{
+  # TODO: Output generated configuration somewhere for transparency
+  imports = map importToml (getTomlFiles ./config);
 
-  environment.systemPackages = flatten (map fromList [
+  environment.systemPackages = let
+    recurse = p: list:
+      let
+        h = p.${head list};
+        t = tail list;
+      in if (t == [ ]) then h else recurse h t;
+    getPkg = packages: string: recurse packages (splitString "." string);
+    fromList = { file, packages }:
+      map (getPkg packages) (fromTOML (readFile file)).packages;
+  in flatten (map fromList [
     {
       file = ./stable.toml;
       packages = pkgs;
