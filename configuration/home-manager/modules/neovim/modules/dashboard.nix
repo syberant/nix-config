9e3d386491c6e695b7ca48f8800cf49625ab7fe5
syberant <sybrand@neuralcoding.com>
2021-07-10 21:21:05 +0200

    Neovim: Add dashboard-nvim

---
+++ configuration/home-manager/modules/neovim/modules/dashboard.nix
@@ -0,0 +1,62 @@
+{ pkgs, ... }:
+
+{
+  vim.g = {
+    # TODO: Get quote working
+    # dashboard_custom_footer = ''call v:lua.get_quote()'';
+
+    dashboard_custom_header = [
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣴⣦⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+      "⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⠿⠿⠿⠿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀"
+      "⠀⠀⠀⠀⣠⣾⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣶⡀⠀⠀⠀⠀"
+      "⠀⠀⠀⣴⣿⣿⠟⠁⠀⠀⠀⣶⣶⣶⣶⡆⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣦⠀⠀⠀"
+      "⠀⠀⣼⣿⣿⠋⠀⠀⠀⠀⠀⠛⠛⢻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣧⠀⠀"
+      "⠀⢸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡇⠀"
+      "⠀⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀"
+      "⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡟⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⣹⣿⣿⠀"
+      "⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀⣰⣿⣿⠏⠀⠀⢻⣿⣿⡄⠀⠀⠀⠀⠀⠀⣿⣿⡿⠀"
+      "⠀⢸⣿⣿⡆⠀⠀⠀⠀⣴⣿⡿⠃⠀⠀⠀⠈⢿⣿⣷⣤⣤⡆⠀⠀⣰⣿⣿⠇⠀"
+      "⠀⠀⢻⣿⣿⣄⠀⠀⠾⠿⠿⠁⠀⠀⠀⠀⠀⠘⣿⣿⡿⠿⠛⠀⣰⣿⣿⡟⠀⠀"
+      "⠀⠀⠀⠻⣿⣿⣧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠏⠀⠀⠀"
+      "⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⣄⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⠟⠁⠀⠀⠀⠀"
+      "⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀"
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    ];
+    dashboard_enable_session = false;
+    dashboard_default_executive = "telescope";
+    dashboard_custom_section = {
+      "oldfiles" = {
+        description = [ " Open History                                 " ];
+        command = "Telescope oldfiles";
+      };
+      "find_files" = {
+        description = [ " Find File                             SPC f o" ];
+        command = "Telescope find_files";
+      };
+      "live_grep" = {
+        description = [ " Live Grep                             SPC f /" ];
+        command = "Telescope live_grep";
+      };
+      "new_file" = {
+        description = [ " New File                                     " ];
+        command = "enew";
+      };
+      "lazygit" = {
+        description = [ " lazygit                               SPC g g" ];
+        command = "LazyGit";
+      };
+    };
+  };
+
+  output.config_file = ''
+    lua << EOF
+      function _G.get_quote()
+        local output = vim.fn.system("fortune")
+        return vim.split(output, '\n', false)
+      end
+    EOF
+  '';
+
+  output.plugins = with pkgs.vimPlugins; [ dashboard-nvim ];
+  # output.path.path = with pkgs; [ fortune ];
+}
