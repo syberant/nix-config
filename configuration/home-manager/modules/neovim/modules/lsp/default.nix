07267208eee481ce13a47b0572a53915a271a2ec
syberant <sybrand@neuralcoding.com>
2021-07-10 12:01:22 +0200

    Neovim: Add lsp-status.nvim

--- configuration/home-manager/modules/neovim/modules/lsp/default.nix
+++ configuration/home-manager/modules/neovim/modules/lsp/default.nix
@@ -24,6 +24,11 @@ with lib;
     "K" = { command = "<cmd>lua vim.lsp.buf.hover()<cr>"; };
   };
 
+  vim.g.lightline.component_expand.lsp_status = "LspStatus";
+  vim.g.lightline.active.right = mkAfter [
+      [ "lsp_status" ]
+  ];
+
   # https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/2
   output.makeWrapper =
     "--set RUST_SRC_PATH ${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
@@ -32,6 +37,22 @@ with lib;
     set shortmess+=c
 
     lua <<EOF
+    -- lsp_status
+    local lsp_status = require('lsp-status')
+    lsp_status.config({
+      status_symbol = "",
+      current_function = false,
+      -- indicator_errors = 'E',
+      -- indicator_warnings = 'W',
+      -- indicator_info = 'i',
+      -- indicator_hint = '?',
+      -- indicator_ok = '好',
+    })
+    lsp_status.register_progress()
+
+    function on_attach(client)
+      lsp_status.on_attach(client)
+    end
 
     -- nvim_lsp object
     local nvim_lsp = require'lspconfig'
@@ -49,10 +70,22 @@ with lib;
     -- Enable rust_analyzer
     ${optionalString config.languages.rust.enable ''
       nvim_lsp.rust_analyzer.setup({
+        on_attach = on_attach,
         cmd = { "${pkgs.rust-analyzer}/bin/rust-analyzer" },
+        capabilities = lsp_status.capabilities,
       })
     ''}
     EOF
+
+    " Statusline
+    function! LspStatus() abort
+      if luaeval('#vim.lsp.buf_get_clients() > 0')
+        return luaeval("require('lsp-status').status()")
+      endif
+      return ""
+    endfunction
+    autocmd InsertLeave,BufEnter,BufWritePost * call lightline#update()
+    autocmd User LspDiagnosticsChanged call lightline#update()
 
     " Show diagnostic popup on cursor hold
     autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
@@ -65,6 +98,7 @@ with lib;
   output.plugins = with pkgs.vimPlugins; [
     nvim-lspconfig
     lsp_extensions-nvim
+    lsp-status-nvim
   ];
 
   output.path.path = with pkgs; [ cargo rustc rustfmt ];
