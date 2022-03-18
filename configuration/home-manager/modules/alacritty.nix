38eec81914b6901e69046cf48d6c64d09f7c4681
syberant <sybrand@neuralcoding.com>
2022-03-18 17:13:00 +0100

    Install alacritty

---
+++ configuration/home-manager/modules/alacritty.nix
@@ -0,0 +1,9 @@
+{
+  programs.alacritty = {
+    enable = true;
+
+    # Declare settings here.
+    # See https://github.com/alacritty/alacritty/blob/master/alacritty.yml
+    settings = { };
+  };
+}
