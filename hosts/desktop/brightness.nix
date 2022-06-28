f1d89eb182619f802b41aee29eda6d60c8af68c6
syberant <sybrand@neuralcoding.com>
2022-06-28 12:39:02 +0200

    nixos-desktop: Enable ddcci driver for monitor backlight

--- hosts/desktop/brightness.nix
+++ hosts/desktop/brightness.nix
@@ -1,12 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-        ddcutil
-  ];
-
-  boot.kernelModules = [ "i2c-dev" ];
 
   # For setting brightness to x (0<=x<=100):
   # sudo ddcutil setvcp 10 x
+  # environment.systemPackages = with pkgs; [ ddcutil ];
+
+  boot = {
+    # Use a kernel driver, presents backlight(s) in /sys/class/backlight
+    # Compatible with e.g. brightnessctl utility
+    extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
+
+    # Enable debug messages
+    # extraModprobeConfig = ''
+      # options ddcci dyndbg
+      # options ddcci-backlight dyndbg
+    # '';
+  };
+
+  # Temporarily remap F{1,2} to XF86MonBrightness{Down,Up}
+  gui.keyboard.key_mappings = [
+     "67 = XF86MonBrightnessDown"
+     "68 = XF86MonBrightnessUp"
+  ];
 }
