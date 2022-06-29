43683b6c4a7ea8a9a63a5e31dca2e6adacccd097
syberant <sybrand@neuralcoding.com>
2022-06-29 12:21:09 +0200

    nixos-desktop: Force-detect ddcci display

--- hosts/desktop/brightness.nix
+++ hosts/desktop/brightness.nix
@@ -1,15 +1,11 @@
 { config, pkgs, ... }:
 
 {
-
-  # For setting brightness to x (0<=x<=100):
-  # sudo ddcutil setvcp 10 x
-  # environment.systemPackages = with pkgs; [ ddcutil ];
-
   boot = {
     # Use a kernel driver, presents backlight(s) in /sys/class/backlight
     # Compatible with e.g. brightnessctl utility
     extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
+    kernelModules = [ "ddcci" ];
 
     # Enable debug messages
     # extraModprobeConfig = ''
@@ -18,9 +14,27 @@
     # '';
   };
 
+  # The ddcci kernel driver does not currently autodetect my ddcci screen.
+  # This is most likely the fault of the proprietary nvidia driver,
+  # but is solved by forcing a detect with a systemd service.
+  systemd.services."ddcci-force-detect" = {
+    description = "Forces detection of i2c-5 for the ddcci display backlight driver.";
+    documentation = [
+      "https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/issues/7"
+      "https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/issues/2"
+    ];
+
+    # Automatically run on bootup, hopefully after gpu is done initializing...
+    wantedBy = [ "multi-user.target" ];
+    after = [ "multi-user.target" ];
+
+    # TODO: ensure running on hibernate resume
+
+    path = with pkgs; [ coreutils ];
+    script = "echo 'ddcci 0x37' > /sys/bus/i2c/devices/i2c-5/new_device";
+  };
+
   # Temporarily remap F{1,2} to XF86MonBrightness{Down,Up}
-  gui.keyboard.key_mappings = [
-     "67 = XF86MonBrightnessDown"
-     "68 = XF86MonBrightnessUp"
-  ];
+  gui.keyboard.key_mappings =
+    [ "67 = XF86MonBrightnessDown" "68 = XF86MonBrightnessUp" ];
 }
