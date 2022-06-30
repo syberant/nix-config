58d0cd7e5f89b433ef456d78d7b5dbc8a036705b
syberant <sybrand@neuralcoding.com>
2022-06-30 20:53:37 +0200

    Alacritty: Make configuration

--- configuration/home-manager/modules/alacritty.nix
+++ configuration/home-manager/modules/alacritty.nix
@@ -4,6 +4,15 @@
 
     # Declare settings here.
     # See https://github.com/alacritty/alacritty/blob/master/alacritty.yml
-    settings = { };
+    settings = {
+      window.opacity = 0.96;
+      font = {
+        normal.family = "Source Code Pro";
+        bold.family = "Source Code Pro";
+        italic.family = "Source Code Pro";
+        bold_italic.family = "Source Code Pro";
+        size = 12;
+      };
+    };
   };
 }
