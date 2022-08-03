bd22b1eea2841c2155e515ed21adc63ce295cb51
syberant <sybrand@neuralcoding.com>
2022-08-03 20:44:21 +0200

    Uninstall nnn

--- configuration/home-manager/modules/bash.nix
+++ configuration/home-manager/modules/bash.nix
@@ -9,17 +9,6 @@
     initExtra = ''
       # Use starship for prompt
       eval "$(starship init bash)"
-
-      # Quit to current directory in nnn
-      export NNN_TMPFILE="/tmp/nnn"
-      n() {
-          nnn "$@"
-
-          if [ -f $NNN_TMPFILE ]; then
-              . $NNN_TMPFILE
-              rm -f $NNN_TMPFILE > /dev/null
-          fi
-      }
     '';
 
     shellAliases = {
