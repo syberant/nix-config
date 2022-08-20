18c7fb65a3efe1bdd6070dd4c3c815dfa30472fd
syberant <sybrand@neuralcoding.com>
2022-08-20 11:32:03 +0200

    neovim: Add LSP keybindings

--- configuration/home-manager/modules/neovim/modules/keybindings.nix
+++ configuration/home-manager/modules/neovim/modules/keybindings.nix
@@ -70,6 +70,32 @@
         };
       };
 
+      ### LSP commands
+      s = {
+        # name = "lsp";
+
+        K = {
+          command = "<cmd>lua vim.lsp.buf.hover()<cr>";
+          label = "Hover";
+        };
+        d = {
+          command = "<cmd>lua vim.lsp.buf.definition()<cr>";
+          label = "Definition";
+        };
+        D = { command = "<cmd>lua vim.lsp.buf.declaration()<cr>"; label = "Declaration"; };
+        i = { command = "<cmd>lua vim.lsp.buf.implementation()<cr>"; label = "Implementation"; };
+        o = { command = "<cmd>lua vim.lsp.buf.type_definition()<cr>"; label = "Type Definition"; };
+        r = { command = "<cmd>lua vim.lsp.buf.references()<cr>"; label = "References"; };
+
+        c = { command = "<cmd>lua vim.lsp.buf.rename()<cr>"; label = "Rename"; };
+        # -- TODO: When in x mode do `range_code_action` instead
+        a = { command = "<cmd>lua vim.lsp.buf.code_action()<cr>"; label = "Code Action"; mode = "n";};
+
+        s = { command = "<cmd>lua vim.diagnostic.open_float()<cr>"; label = "Show Diagnostic"; };
+        "[" = { command = "<cmd>lua vim.diagnostic.goto_prev()<cr>"; label = "Prev Diagnostic"; };
+        "]" = { command = "<cmd>lua vim.diagnostic.goto_next()<cr>"; label = "Next Diagnostic"; };
+      };
+
       ### Miscellaneous
       ";" = {
         mode = "";
