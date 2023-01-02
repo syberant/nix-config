c0ede2862f9311720186a8bb6033121cd5d12eb3
syberant <sybrand@neuralcoding.com>
2023-01-02 13:58:18 +0100

    n-system-scripts: Add update subcommand

--- configuration/n-system-scripts/n-completion.bash
+++ configuration/n-system-scripts/n-completion.bash
@@ -1,3 +1,18 @@
 #/usr/bin/env bash
 
-complete -W "repl search" n
+_n_completions() {
+    case "${#COMP_WORDS[@]}" in
+        "1" | "2")
+            COMPREPLY=($(compgen -W "repl search update" "${COMP_WORDS[1]}"))
+            ;;
+        "3")
+            case "${COMP_WORDS[1]}" in
+                "update")
+                    COMPREPLY=($(compgen -W "stable unstable nur misc own-deps" "${COMP_WORDS[2]}"))
+                    ;;
+            esac
+            ;;
+    esac
+}
+
+complete -F _n_completions n
--- configuration/n-system-scripts/n.sh
+++ configuration/n-system-scripts/n.sh
@@ -6,7 +6,32 @@ case $1 in
         ;;
     "search")
         nix search nixpkgs $2;;
+    "update")
+        cd /etc/nixos
+        case $2 in
+            "stable")
+                nix flake lock --update-input nixpkgs --update-input nixos-hardware --update-input home-manager
+                ;;
+            "unstable")
+                nix flake lock --update-input nixpkgs-git
+                ;;
+            "nur")
+                nix flake lock --update-input NUR
+                ;;
+            "misc")
+                nix flake lock --update-input sops-nix --update-input flake-utils --update-input hosts
+                ;;
+            "own-deps")
+                nix flake lock --update-input secrets --update-input xmonad-sybrand --update-input nix-neovim
+                ;;
+            *)
+                # TODO: Maybe interpret this as a specific dependency and update it?
+                echo "n update: Didn't recognise argument, what should be updated?"
+                exit 1
+        esac
+        ;;
     *)
         echo "Didn't recognise subcommand. Did you make a typo?"
         exit 1
+        ;;
 esac
