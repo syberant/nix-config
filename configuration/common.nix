292c5edc9fc48f94ea0e97ffe22047694914cbb8
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2023-02-22 23:12:13 +0100

    hosts: Have host-specific `system.stateVersion`

--- configuration/common.nix
+++ configuration/common.nix
@@ -20,10 +20,4 @@
   # hardware.opengl.driSupport32Bit = true;
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
 }
