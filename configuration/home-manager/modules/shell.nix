7ec64a92bc05f894cd4f18afb27e77ce15604a7e
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-17 17:20:01 +0200

    Use `bat` as manpager

--- configuration/home-manager/modules/shell.nix
+++ configuration/home-manager/modules/shell.nix
@@ -4,7 +4,10 @@
   home.sessionVariables = {
     VISUAL = "nvim";
     EDITOR = "nvim";
-    # Use neovim as manpager
-    MANPAGER = "nvim +Man!";
+
+    # Use bat as manpager
+    # See: https://github.com/sharkdp/bat#man
+    MANPAGER = "sh -c 'col -bx | bat --language man --plain --theme gruvbox-dark'";
+    MANROFFOPT="-c";
   };
 }
