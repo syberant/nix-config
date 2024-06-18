7b1af8630cb341e4fc8f01f450d1b4ffaa3fd272
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-18 08:52:43 +0200

    Use colourised `less` as MANPAGER

--- configuration/home-manager/modules/shell.nix
+++ configuration/home-manager/modules/shell.nix
@@ -5,9 +5,9 @@
     VISUAL = "nvim";
     EDITOR = "nvim";
 
-    # Use bat as manpager
-    # See: https://github.com/sharkdp/bat#man
-    MANPAGER = "sh -c 'col -bx | bat --language man --plain --theme gruvbox-dark'";
-    MANROFFOPT="-c";
+    # Colourise `less` output for man pages.
+    # Ideally I'd use something like `bat` but it is broken for man pages currently and does not colourise correctly.
+    MANPAGER = "less -R --use-color -Dd+r -Du+b";
+    MANROFFOPT="-P -c";
   };
 }
