9a6a3517978d07809776875f2e21571776a03b8a
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-21 22:46:06 +0200

    Auto-start tmux

--- configuration/home-manager/modules/tmux.nix
+++ configuration/home-manager/modules/tmux.nix
@@ -1,6 +1,30 @@
 { pkgs, ... }:
 
 {
+  # From: https://github.com/tmux-plugins/tmux-continuum/blob/master/scripts/handle_tmux_automatic_start/systemd_enable.sh
+  # TODO: Fix flakiness and add save script
+  systemd.user.services.tmux-server = {
+    Unit = {
+      Description = "tmux default session (detached)";
+      Documentation = [ "man:tmux(1)" ];
+
+    };
+
+	Service = {
+      Type="forking";
+      Environment=["DISPLAY=:0"];
+      ExecStart="${pkgs.tmux}/bin/tmux new-session -d";
+
+      # ExecStop="${pkgs.tmuxPlugins.resurrect}/scripts/save.sh";
+      ExecStop="${pkgs.tmux}/bin/tmux kill-server";
+      KillMode="control-group";
+
+      RestartSec=2;
+    };
+
+	Install.WantedBy=["default.target"];
+  };
+
   programs.tmux = {
     enable = true;
 
