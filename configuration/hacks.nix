581c901b1ad3801795fe7e6a0ca66231a0264f00
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-21 15:49:23 +0200

    Clarify networking setup

--- configuration/hacks.nix
+++ configuration/hacks.nix
@@ -1,8 +1,10 @@
 { lib, ... }:
 
 {
-  # FIXME: This service fails while everything is fine.
+  # FIXME: This service fails on `nixos-rebuild switch` while everything is fine.
   # See https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
-  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+
+  # Managed by NetworkManager instead.
+  systemd.network.wait-online.enable = lib.mkForce false;
 }
