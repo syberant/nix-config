21a06499ef71a60f6f71b2d6d5b4e1b045a23a35
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-22 09:51:21 +0200

    TOML: Allow importing `.nix` files as well

--- toml/default.nix
+++ toml/default.nix
@@ -3,9 +3,28 @@
 with lib;
 with pkgs.nur.repos.syberant.lib;
 
-{
-  # TODO: Output generated configuration somewhere for transparency
-  imports = map importToml (getTomlFiles ./config);
+let
+  getFiles = { dir, suffixes ? [], recursive ? false, allow_default ? true }:
+    let
+      hasDefault = d: hasAttr "default.nix" (builtins.readDir (dir + "/${d}"));
+      isImportable = name: kind:
+        if kind == "directory" then
+          recursive || (allow_default && hasDefault name)
+        else
+          lists.any (v: hasSuffix v name) suffixes;
+      files = attrNames (filterAttrs isImportable (builtins.readDir dir));
+    in map (f: dir + "/${f}") files;
+  getAllFiles = dir: getFiles { inherit dir; suffixes = [ "nix" "toml" ]; recursive = true; };
+
+  importFile = file:
+    if hasSuffix "toml" file then
+        importToml file
+      else if hasSuffix "nix" file then
+        file
+      else throw "Unrecognized module file type '${file}', only 'nix' and 'toml' are allowed.";
+  importFiles = dir: map importFile (getAllFiles dir);
+in {
+  imports = importFiles ./config;
 
   environment.systemPackages = let
     ofPkgs = p: getAttrFromPath (splitString "." p) pkgs;
