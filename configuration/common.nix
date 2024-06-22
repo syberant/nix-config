096edec1b5f814e4acc041f14d3f172251642e71
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-22 10:23:42 +0200

    Move TOML `config` into `configuration` directory

--- configuration/common.nix
+++ configuration/common.nix
@@ -2,10 +2,33 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ self, ... }:
+{ self, lib, pkgs, ... }:
 
-{
-  imports = [
+with lib;
+with pkgs.nur.repos.syberant.lib;
+
+let
+  getFiles = { dir, suffixes ? [], recursive ? false, allow_default ? true }:
+    let
+      hasDefault = d: hasAttr "default.nix" (builtins.readDir (dir + "/${d}"));
+      isImportable = name: kind:
+        if kind == "directory" then
+          recursive || (allow_default && hasDefault name)
+        else
+          lists.any (v: hasSuffix v name) suffixes;
+      files = attrNames (filterAttrs isImportable (builtins.readDir dir));
+    in map (f: dir + "/${f}") files;
+  getAllFiles = dir: getFiles { inherit dir; suffixes = [ "nix" "toml" ]; recursive = true; };
+
+  importFile = file:
+    if hasSuffix "toml" file then
+        importToml file
+      else if hasSuffix "nix" file then
+        file
+      else throw "Unrecognized module file type '${file}', only 'nix' and 'toml' are allowed.";
+  importFiles = dir: map importFile (getAllFiles dir);
+in {
+  imports = importFiles ./config ++ [
     ../desktop-environment
     ../home-manager
     ../toml
@@ -13,6 +36,7 @@
     ./secrets
     ./n-system-scripts
   ];
+
 
   # Let 'nixos-version --json' know about the Git revision of this flake.
   system.configurationRevision = self.rev or "dirty";
