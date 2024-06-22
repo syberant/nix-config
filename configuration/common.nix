32e34e54015429387a48f4bd618f4d07c79cc17e
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-22 16:32:37 +0200

    TOML: Use functions from syberant NUR

--- configuration/common.nix
+++ configuration/common.nix
@@ -8,39 +8,9 @@ with lib;
 with pkgs.nur.repos.syberant.lib;
 
 let
-  getFiles = { dir, suffixes ? [ ], recursive ? false, allow_default ? true }:
-    let
-      hasDefault = d: hasAttr "default.nix" (builtins.readDir (dir + "/${d}"));
-      isImportable = name: kind:
-        if kind == "directory" then
-          recursive || (allow_default && hasDefault name)
-        else
-          lists.any (v: hasSuffix v name) suffixes;
-      files = attrNames (filterAttrs isImportable (builtins.readDir dir));
-    in map (f: dir + "/${f}") files;
-  getAllFiles = dir:
-    getFiles {
-      inherit dir;
-      suffixes = [ "nix" "toml" ];
-      recursive = true;
-    };
+  importFile = importFileWithHandler defaultHandlers;
 
-  importFileWithHandler = methods: file:
-    let
-      unsupportedType = throw
-        "Unrecognized module file type '${file}', allowed: ${attrNames methods}";
-      suffix = findFirst (a: hasSuffix a file) unsupportedType
-        (attrNames methods);
-      method = methods.${suffix} or unsupportedType;
-    in method file;
-  handlers = {
-    toml = importToml;
-    nix = file: import file;
-    json = lib.importJSON;
-  };
-  importFile = importFileWithHandler handlers;
-
-  importFiles = dir: map importFile (getAllFiles dir);
+  importFiles = dir: map importFile (getNixTomlJsonFiles dir);
 in {
   imports = importFiles ./config ++ [
     ../desktop-environment
