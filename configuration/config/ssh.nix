95ac2697d9d60ba98bb40959ee118fe503c7dbf4
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-24 15:02:10 +0200

    ssh: Only allow login of regular users

--- configuration/config/ssh.nix
+++ configuration/config/ssh.nix
@@ -8,14 +8,23 @@
     # Don't use keys in ~/.ssh/authorized_keys
     authorizedKeysInHomedir = false;
 
-    # Disallow password authentication.
-    # passwordAuthentication = false;
-
     # Don't open a port in the firewall.
     # WARN: This means you can't SSH into a computer without a VPN!
     # See: https://fzakaria.com/2020/09/17/tailscale-is-magic-even-more-so-with-nixos.html
     # I use Tailscale for this.
     # openFirewall = false;
+
+    settings = {
+      # Don't allow logging in as root!
+      # Standard hardening procedure.
+      PermitRootLogin = "no";
+
+      # Only allow logging in regular users
+      AllowGroups = [ "users" ];
+
+      # Disallow password authentication.
+      # passwordAuthentication = false;
+    };
   };
 
   # Always allow login with this key
