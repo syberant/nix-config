8a30b0a284a507622726ce21d6bddbee8567067c
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-24 19:43:25 +0200

    Move NixOS settings to `nixos.nix` file
    Previously they were in the `common.nix` file.

--- configuration/common.nix
+++ configuration/common.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ self, lib, pkgs, config, ... }:
+{ lib, pkgs, ... }:
 
 with lib;
 with pkgs.nur.repos.syberant.lib;
@@ -58,30 +58,4 @@ in {
     ./secrets
     ./n-system-scripts
   ];
-
-  # Include a copy of the flake used to build a generation *inside* that generation
-  # Accessible at /run/current-system/sw/share/self-flake/nested/dir
-  environment = {
-    # TODO: Fix mess with nested dir
-    systemPackages = [ (pkgs.runCommand "nixos-configuration-flake" {} "mkdir -p $out/share/self-flake/nested/dir; cp -r ${self}/* $out/share/self-flake/nested/dir") ];
-    pathsToLink = [ "/share/self-flake" ];
-  };
-
-  nix = {
-    # Disable using `nix-channel`
-    channel.enable = false;
-  };
-
-  # Let 'nixos-version --json' know about the Git revision of this flake.
-  system.configurationRevision = self.rev or "dirty";
-
-  system.nixos = {
-    # Set label in GRUB to first 7 characters of the git hash of the last commit.
-    label = let
-      abbreviate = s: concatStrings (take 7 (stringToCharacters s));
-    in abbreviate config.system.configurationRevision;
-
-    # Use tags to keep track of features, these will also be shown in GRUB
-    # tags = [ "tag1" "tag2" ];
-  };
 }
---
+++ configuration/config/nixos.nix
@@ -0,0 +1,31 @@
+{ self, config, pkgs, ... }:
+
+with pkgs.lib;
+
+{
+  # Include a copy of the flake used to build a generation *inside* that generation
+  # Accessible at /run/current-system/sw/share/self-flake/nested/dir
+  environment = {
+    # TODO: Fix mess with nested dir
+    systemPackages = [ (pkgs.runCommand "nixos-configuration-flake" {} "mkdir -p $out/share/self-flake/nested/dir; cp -r ${self}/* $out/share/self-flake/nested/dir") ];
+    pathsToLink = [ "/share/self-flake" ];
+  };
+
+  nix = {
+    # Disable using `nix-channel`
+    channel.enable = false;
+  };
+
+  # Let 'nixos-version --json' know about the Git revision of this flake.
+  system.configurationRevision = self.rev or "dirty";
+
+  system.nixos = {
+    # Set label in GRUB to first 7 characters of the git hash of the last commit.
+    label = let
+      abbreviate = s: concatStrings (take 7 (stringToCharacters s));
+    in abbreviate config.system.configurationRevision;
+
+    # Use tags to keep track of features, these will also be shown in GRUB
+    # tags = [ "tag1" "tag2" ];
+  };
+}
