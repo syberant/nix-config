dc5f80a0f1b02a846c19bb7ef9df1a2f412dc300
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-06-25 23:35:15 +0200

    Farewell `xcape`! Using `keyd` now.
    It's served me well but is a bit buggy and needs to be rerun sometimes.
    I hope `keyd` is a bit more stable, it's certainly more flexible.

---
+++ configuration/config/keyd.nix
@@ -0,0 +1,11 @@
+{
+  services.keyd = {
+    enable = true;
+    keyboards.default = {
+      ids = [ "*" ];
+      settings.main = {
+        capslock = "overload(control, esc)";
+      };
+    };
+  };
+}
--- modules/gui/keyboard.nix
+++ modules/gui/keyboard.nix
@@ -2,47 +2,22 @@
 
 with lib;
 
-let
-	cfg = config.gui.keyboard;
+let cfg = config.gui.keyboard;
 in {
-	options.gui.keyboard = {
-		control_as_escape = mkOption {
-			type = types.bool;
-			default = true;
-			description = "If enabled, control will act as control when held and as escape when tapped.";
-		};
+  options.gui.keyboard = {
+    key_mappings = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+    };
+  };
 
-        caps_lock_to_control = mkOption {
-            type = types.bool;
-            default = true;
-            description = "If enabled, caps lock will be mapped to control.";
-        };
-
-        key_mappings = mkOption {
-            type = types.listOf types.str;
-            default = [];
-        };
-	};
-
-	config = let
-        control_as_escape = mkIf cfg.control_as_escape {
-            environment.systemPackages = [ pkgs.xcape ];
-
-            services.xserver.displayManager.sessionCommands = ''
-                ${pkgs.xcape}/bin/xcape -e 'Control_L=Escape'
-            '';
-        };
-        caps_lock_to_control = mkIf cfg.caps_lock_to_control {
-            services.xserver.xkb.options = "ctrl:nocaps";
-        };
-        key_mappings =
-            let code = map (a: ''xmodmap -e "keycode '' + a + ''"'') cfg.key_mappings;
-            in {
-                services.xserver.displayManager.sessionCommands = ''PATH="$PATH:${pkgs.xorg.xmodmap}/bin" bash ${pkgs.writeScript "macbook-fix-numbers" (builtins.concatStringsSep "\n" code)}'';
-            };
-    in mkMerge [
-        control_as_escape
-        caps_lock_to_control
-        key_mappings
-    ];
+  config =
+    let code = map (a: ''xmodmap -e "keycode '' + a + ''"'') cfg.key_mappings;
+    in {
+      services.xserver.displayManager.sessionCommands = ''
+        PATH="$PATH:${pkgs.xorg.xmodmap}/bin" bash ${
+          pkgs.writeScript "macbook-fix-numbers"
+          (builtins.concatStringsSep "\n" code)
+        }'';
+    };
 }
