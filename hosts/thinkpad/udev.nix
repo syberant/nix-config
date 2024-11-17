b043078022a8e199015cdee5d8b630a1a141a86c
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2024-11-17 17:02:22 +0100

    udev: Add automatic wlan on/off events

--- hosts/thinkpad/udev.nix
+++ hosts/thinkpad/udev.nix
@@ -3,17 +3,31 @@
 # https://superuser.com/questions/357890/how-to-block-sites-with-iptables
 
 let
-  scriptAdd = pkgs.writeScript "usb-add" ''
+  # Make a script
+  # TODO: Make a pure PATH?
+  # TODO: Debounce for rapid repeated udev events?
+  mkScript = name: text: pkgs.writeScript name ''
     #!${pkgs.bash}/bin/bash
 
+    ${text}
+  '';
+
+  scriptAdd = mkScript "usb-add" ''
     ${pkgs.iptables}/bin/iptables -R distracting 1 -j ACCEPT
     echo "Custom USB key inserted, distracting websites allowed" > /dev/kmsg
   '';
-  scriptRemove = pkgs.writeScript "usb-remove" ''
-    #!${pkgs.bash}/bin/bash
-
+  scriptRemove = mkScript "usb-remove" ''
     ${pkgs.iptables}/bin/iptables -R distracting 1 -j REJECT
     echo "Custom USB key removed, distracting websites blocked" > /dev/kmsg
+  '';
+
+  wifiOff = mkScript "wifi-off" ''
+    echo "Custom USB key removed, turning wifi off" > /dev/kmsg
+    rfkill block wlan
+  '';
+  wifiOn = mkScript "wifi-on" ''
+    echo "Custom USB key inserted, turning wifi on" > /dev/kmsg
+    rfkill unblock wlan
   '';
 in {
   networking.firewall.extraCommands = ''
@@ -31,9 +45,21 @@ in {
   iptables -A OUTPUT -p tcp -m string --string "bbc.com"         --algo kmp -j distracting
   iptables -A OUTPUT -p tcp -m string --string "arstechnica.com" --algo kmp -j distracting
   '';
+
+
+  # Monitor udev events with `udevadm monitor --subsystem-match=usb --property --udev`
+  # You can use this to find new vendor and model id's.
   services.udev.extraRules = ''
     # Philips USB stick
     ACTION=="add", SUBSYSTEMS=="usb", ATTRS{manufacturer}=="Philips", ATTRS{idVendor}=="13fe", ATTRS{idProduct}=="5200", RUN+="${scriptAdd}"
     ACTION=="remove", SUBSYSTEMS=="usb", ENV{ID_VENDOR_ID}=="13fe", ENV{ID_MODEL_ID}=="5200", RUN+="${scriptRemove}"
+
+    # Kingston 4GB stick
+    # ACTION=="add", SUBSYSTEMS=="usb", ATTRS{manufacturer}=="Kingston", ATTRS{idVendor}=="0951", ATTRS{idProduct}=="1642", RUN+="${wifiOn}"
+    # ACTION=="remove", SUBSYSTEMS=="usb", ENV{ID_VENDOR_ID}=="0951", ENV{ID_MODEL_ID}=="1642", RUN+="${wifiOff}"
+
+    # 8GB "radio" stick
+    ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="058f", ATTRS{idProduct}=="6387", RUN+="${wifiOn}"
+    ACTION=="remove", SUBSYSTEMS=="usb", ENV{ID_VENDOR_ID}=="058f", ENV{ID_MODEL_ID}=="6387", RUN+="${wifiOff}"
   '';
 }
