7e54ed3809a4efdb943df4a629e7032729e0df94
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2025-01-27 14:04:58 +0100

    Install open-webui

--- hosts/thinkpad/main.nix
+++ hosts/thinkpad/main.nix
@@ -9,6 +9,19 @@
   services.ollama = {
     enable = true;
   };
+  services.open-webui = {
+    enable = true;
+
+    port = 8086;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+      WEBUI_AUTH = "False";
+    };
+  };
 
   programs.steam.enable = true;
   environment.systemPackages = with pkgs; [
