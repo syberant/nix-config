0a5b4101f8b543e5e73262b16b4fadb19413218b
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2025-07-11 18:17:59 +0200

    hosts/macbook: Add nixos-thinkpad as host.

--- hosts/macbook/syncthing.nix
+++ hosts/macbook/syncthing.nix
@@ -16,19 +16,24 @@
       introducer = true;
     };
 
-    settings.folders = {
-      "/home/${user}/Documents/Radboud_Universiteit_1" = {};
-      "/home/${user}/Documents/Radboud_Universiteit_2" = {};
-      "/home/${user}/Documents/Radboud_Universiteit_3" = {};
-      "/home/${user}/Documents/CGU_5E" = {};
-      "/home/${user}/Documents/CGU_6E" = {};
-      "/home/${user}/Documents/Persoonlijk" = {};
-      "/home/${user}/Documents/Programmeren" = {};
-      "/home/${user}/Literatuur" = {};
-      "/home/${user}/Media/Music" = {};
-      "/home/${user}/.password-store" = {};
-      "/home/${user}/Notities" = {};
-      "/home/${user}/Agenda" = {};
+    settings.devices.nixos-thinkpad = {
+      autoAcceptFolders = true;
+      id = "EENKZIW-CXAZDYC-VN7JWIB-RULDRBF-ZCJ7AF2-OCDTG6W-NC2HOUO-AVBAOAP";
     };
+
+    # settings.folders = {
+    #   "/home/${user}/Documents/Radboud_Universiteit_1" = {};
+    #   "/home/${user}/Documents/Radboud_Universiteit_2" = {};
+    #   "/home/${user}/Documents/Radboud_Universiteit_3" = {};
+    #   "/home/${user}/Documents/CGU_5E" = {};
+    #   "/home/${user}/Documents/CGU_6E" = {};
+    #   "/home/${user}/Documents/Persoonlijk" = {};
+    #   "/home/${user}/Documents/Programmeren" = {};
+    #   "/home/${user}/Media/Literatuur" = {};
+    #   "/home/${user}/Media/Music" = {};
+    #   "/home/${user}/.password-store" = {};
+    #   "/home/${user}/Notities" = {};
+    #   "/home/${user}/Agenda" = {};
+    # };
   };
 }
