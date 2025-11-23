f9d46d5a7315d4c69ad7892ae6498ab7424a42ce
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2025-11-23 09:52:07 +0100

    Update WASM binfmt_misc to support `wasip2`

--- configuration/config/binfmt-wasm.nix
+++ configuration/config/binfmt-wasm.nix
@@ -7,15 +7,13 @@
     # What capabilities should be given to WASM guest?
     # How should those capabilities be configured?
     # - Environment variables?
-    # - Prepend WASM file with capabilities JSON?
+    # - Store capabilities we should give in an xattr?
     interpreter = "${pkgs.wasmtime}/bin/wasmtime";
     wrapInterpreterInShell = false;
 
     # Recognition based on file extension is way too fragile, recognise magic bytes instead.
     recognitionType = "magic";
-    # Will intentionally break when/if a version 2 of the format comes out.
-    # See https://webassembly.github.io/spec/core/binary/modules.html#binary-module
-    magicOrExtension = "\\x00asm\\x01\\x00\\x00\\x00";
+    magicOrExtension = "\\x00asm";
 
     # Set to true if you want to be able to run a WASM setuid binary and you also trust your WASM runtime
     matchCredentials = false;
