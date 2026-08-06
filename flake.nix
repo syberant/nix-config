198deebace656b867b89ff20d604a3e6e7b74c62
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2026-08-06 08:44:48 +0200

    Update to NixOS 26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-git.url = "github:NixOS/nixpkgs";
 
 
@@ -14,7 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       # inputs.nixpkgs.follows = "nixpkgs-git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
